9c2435e08c1ccdd982f02828963f4a56faa9550a
Natalia <chu.cuka@yandex.ru>
2020-07-20 20:32:25 +0300

    style: change color days where tasks finished

--- src/components/calendar/Day.tsx
+++ src/components/calendar/Day.tsx
@@ -17,6 +17,14 @@ export default class Day extends VueComponent<Props> {
         return this.$store.getters.getCurrentDay === this.day;
     }
 
+    get isFinishedTasks() {
+        const tasks = this.$store.getters.getTasks(this.day);
+        if (!tasks.length) return false;
+
+        const notFinishedTasks = tasks.filter((value: any) => !value.checked);
+        return !notFinishedTasks.length;
+    }
+
     get isWithTasks () {
         return this.$store.getters.getTaskCount(this.day) > 0;
     }
@@ -34,6 +42,7 @@ export default class Day extends VueComponent<Props> {
                     styles.day,
                     this.isToday() ? styles.today : '',
                     this.isWithTasks ? styles.tasks : '',
+                    this.isFinishedTasks ? styles.finished: ''
                 ]}
             >
                 {this.day}
